1884501861f5dba753539f28f945add96543be15
chobg486 <chobg486@nate.com>
2017-11-08 01:36:42 +0900

    nav logout update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
 import { Observable } from 'rxjs/Observable';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
@@ -9,8 +11,13 @@ import { Observable } from 'rxjs/Observable';
 })
 export class AppComponent {
   items: Observable<any[]>;
-  constructor(db: AngularFireDatabase) {
+  constructor(db: AngularFireDatabase, public afAuth: AngularFireAuth) {
     this.items = db.list('items').valueChanges();
   }
-
+  login() {
+    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  }
+  logout() {
+    this.afAuth.auth.signOut();
+  }
 }
